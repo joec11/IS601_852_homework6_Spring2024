776ce4e5d0ac6e16147f57caa8a7a9d634361e82
joec11 <jc625@njit.edu>
2024-02-13 19:24:45 -0500

    Modified test_calculator.py to import the Calculator class and use the class methods to perform calculations.

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,18 +1,18 @@
-'''My Calculator Test'''
-from calculator import add, subtract, multiply, divide
+'''My Calculator Test with the Calculator class'''
+from calculator import Calculator
 
 def test_addition():
     '''Test that the addition function works.'''
-    assert add(2, 2) == 4
+    assert Calculator.add(2, 2) == 4
 
 def test_subtraction():
     '''Test that the subtraction function works.'''
-    assert subtract(2, 2) == 0
+    assert Calculator.subtract(2, 2) == 0
 
 def test_multiplication():
     '''Test that the multiplication function works.'''
-    assert multiply(3, 4) == 12
+    assert Calculator.multiply(3, 4) == 12
 
 def test_division():
     '''Test that the division function works.'''
-    assert divide(18, 6) == 3
+    assert Calculator.divide(18, 6) == 3
