077986549cb7889a8b1ebd89af9f56f9ea667e61
joec11 <jc625@njit.edu>
2024-03-09 13:58:26 -0500

    Modified app/__init__.py to configure logging and load environment variables from a .env file.

--- app/__init__.py
+++ app/__init__.py
@@ -1,28 +1,94 @@
+import os
 import pkgutil
 import importlib
-from app.commands import CommandHandler
-from app.commands import Command
+from app.commands import CommandHandler, Command
+from dotenv import load_dotenv, dotenv_values
+import logging
+import logging.config
+from decimal import Decimal, InvalidOperation
 
 class App:
     def __init__(self): # Constructor
+        os.makedirs('logs', exist_ok=True)
+        self.configure_logging()
+        load_dotenv()
+        self.settings = self.load_environment_variables()
+        self.settings.setdefault('ENVIRONMENT', 'PRODUCTION')
         self.command_handler = CommandHandler()
 
+    def configure_logging(self):
+        """Configure Logging to allow logging application activity"""
+        logging_conf_path = 'logging.conf'
+        if os.path.exists(logging_conf_path):
+            logging.config.fileConfig(logging_conf_path, disable_existing_loggers=False)
+        else:
+            logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+        logging.info("Logging configured.")
+    
+    def load_environment_variables(self):
+        """Load the environment variables from the env file"""
+        if os.path.exists(".env"):
+            settings = dotenv_values(".env")
+            logging.info("Environment variables loaded.")
+            return settings
+        else:
+            logging.warning(".env file does not exist. Environment variables not loaded."); \
+            return None
+
+    def get_environment_variable(self, env_var: str = 'ENVIRONMENT'):
+        """Get environment variable"""
+        return self.settings.get(env_var, None)
+
     def load_plugins(self):
-        # Dynamically load all plugins in the plugins directory
+        """Load Plugins"""
         plugins_package = 'app.plugins'
-        for _, plugin_name, is_pkg in pkgutil.iter_modules([plugins_package.replace('.', '/')]):
-            if is_pkg:  # Ensure it's a package
-                plugin_module = importlib.import_module(f'{plugins_package}.{plugin_name}')
-                for item_name in dir(plugin_module):
-                    item = getattr(plugin_module, item_name)
-                    try:
-                        if issubclass(item, (Command)):  # Assuming a BaseCommand class exists
-                            self.command_handler.register_command(plugin_name, item())
-                    except TypeError:
-                        continue  # If item is not a class or unrelated class, just ignore
+        plugins_path = plugins_package.replace('.', '/')
+        if not os.path.exists(plugins_path):
+            logging.warning(f"Plugins directory '{plugins_path}' not found."); \
+            return
+        for _, plugin_name, is_pkg in pkgutil.iter_modules([plugins_path]):
+            if is_pkg:
+                try:
+                    plugin_module = importlib.import_module(f'{plugins_package}.{plugin_name}')
+                    self.register_plugin_commands(plugin_module, plugin_name)
+                except ImportError as e:
+                    logging.error(f"Error importing plugin {plugin_name}: {e}")
+
+    def register_plugin_commands(self, plugin_module, plugin_name):
+        """Register the plugin commands"""
+        for item_name in dir(plugin_module):
+            item = getattr(plugin_module, item_name)
+            if isinstance(item, type) and issubclass(item, Command) and item is not Command:
+                # Command names are now explicitly set to the plugin's folder name
+                self.command_handler.register_command(plugin_name, item())
+                logging.info(f"Command '{plugin_name}' from plugin '{plugin_name}' registered.")
 
     def start(self):
+        """Start method"""
         self.load_plugins()
-        print("Type 'exit' to exit.")
-        while True:  #REPL Read, Evaluate, Print, Loop
-            self.command_handler.execute_command(input(">>> ").strip())
+        logging.info("Application started. Type 'exit' to exit.")
+        while True:
+            cmd_input = input(">>> ").strip().split()
+            try:
+                assert 0 < len(cmd_input) <= 3
+
+                a_decimal, b_decimal = (map(Decimal, cmd_input[:2]) if len(cmd_input) > 2 else (Decimal(0.0), Decimal(1.0)))
+                command = cmd_input[-1]
+
+                result = self.command_handler.execute_command(a_decimal, b_decimal, command)
+
+                if (len(cmd_input) > 2 and result is not None):
+                    logging.info(f"The result of {a_decimal} {command} {b_decimal} is equal to {result}"); \
+                    print(f"The result of {a_decimal} {command} {b_decimal} is equal to {result}")
+            except AssertionError:
+                logging.error("Usage: <number1> <number2> <operation/command> | <command>"); \
+                print("Usage: <number1> <number2> <operation/command> | <command>")
+            except KeyError:  # Assuming execute_command raises KeyError for unknown commands
+                logging.error(f"Unknown command: {cmd_input}"); \
+                print(f"Unknown command: {cmd_input}")
+            except InvalidOperation:
+                logging.error("Invalid Operation"); \
+                print("Invalid Operation")
+            except ValueError as e:
+                logging.error(e); \
+                print(e)
