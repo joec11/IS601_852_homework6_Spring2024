5907dd811f54423f9159b7c879f816c073accebd
joec11 <jc625@njit.edu>
2024-03-09 13:51:07 -0500

    Modified app/plugins/exit/__init__.py to contain logging functionality.

--- app/plugins/exit/__init__.py
+++ app/plugins/exit/__init__.py
@@ -1,6 +1,9 @@
+import logging
 import sys
 from app.commands import Command
 
 class ExitCommand(Command):
     def execute(self, *args):
+        logging.info("Exiting...")
+        logging.info("Application shutdown.")
         sys.exit("Exiting...")
