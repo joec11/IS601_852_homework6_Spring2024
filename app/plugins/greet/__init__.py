a62dc7cb41fcc54496f326b7219502e742bf3fa9
joec11 <jc625@njit.edu>
2024-03-09 13:52:16 -0500

    Modified app/plugins/greet/__init__.py to contain logging functionality.

--- app/plugins/greet/__init__.py
+++ app/plugins/greet/__init__.py
@@ -1,5 +1,7 @@
+import logging
 from app.commands import Command
 
 class GreetCommand(Command):
     def execute(self, *args):
-        print(f'Hello! You entered the \'greet\' command.')
+        logging.info("Hello! You entered the \'greet\' command.")
+        print(f"Hello! You entered the \'greet\' command.")
